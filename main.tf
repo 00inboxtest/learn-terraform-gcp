752e32103f5f1cfdf8068233c5e0b218226ace76
Diwaker <rajpal.diwaker@capgemini.com>
2021-12-02 13:51:56 +0530

    code push

--- main.tf
+++ main.tf
@@ -12,9 +12,44 @@ provider "google" {
 
   project = "custom-valve-332208"
   region  = "us-central1"
-  zone    = "us-central1-c"
+  zone    = "us-west4-b"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "us-west4-b"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  #scratch_disk {
+   # interface = "SCSI"
+ # }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    # email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
